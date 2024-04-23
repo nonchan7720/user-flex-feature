aaaa4e7fa9aee14a11a35d29235d22e75e59fc05
nonchan7720 <nozomi_nishinohara@n-creativesystem.com>
2024-04-23 23:12:43 +0900

    fix service i/f

--- pkg/services/feature/implement.go
+++ pkg/services/feature/implement.go
@@ -7,12 +7,15 @@ import (
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/nonchan7720/user-flex-feature/pkg/container"
-	"github.com/nonchan7720/user-flex-feature/pkg/domain/feature"
+	domain_feature "github.com/nonchan7720/user-flex-feature/pkg/domain/feature"
+	"github.com/nonchan7720/user-flex-feature/pkg/infrastructure/config"
 	"github.com/nonchan7720/user-flex-feature/pkg/infrastructure/config/validator"
+	"github.com/nonchan7720/user-flex-feature/pkg/infrastructure/feature"
 	"github.com/nonchan7720/user-flex-feature/pkg/infrastructure/feature/retriever"
+	"github.com/nonchan7720/user-flex-feature/pkg/infrastructure/storage/cache"
+	"github.com/nonchan7720/user-flex-feature/pkg/infrastructure/storage/serialize"
 	"github.com/nonchan7720/user-flex-feature/pkg/services/feature/internal"
 	"github.com/samber/do"
-	ffclient "github.com/thomaspoignant/go-feature-flag"
 	"github.com/thomaspoignant/go-feature-flag/model"
 	ff_retriever "github.com/thomaspoignant/go-feature-flag/retriever"
 )
@@ -22,71 +25,115 @@ func init() {
 }
 
 type serviceImpl struct {
-	ff         *ffclient.GoFeatureFlag
+	cfg        *config.Config
+	ff         *feature.Client
 	retrievers []ff_retriever.Retriever
+	cache      cache.Cache[model.RawVarResult]
 }
 
 func Provide(i *do.Injector) (Service, error) {
-	ff := do.MustInvoke[*ffclient.GoFeatureFlag](i)
+	cfg := do.MustInvoke[*config.Config](i)
+	client := do.MustInvoke[*feature.Client](i)
 	retrievers := do.MustInvoke[[]ff_retriever.Retriever](i)
-	return newService(retrievers, ff), nil
+	cacheBackend := do.MustInvoke[cache.Backend](i)
+	cache := cache.NewCache(cacheBackend, &serialize.JsonSerializer[model.RawVarResult]{})
+	return newService(cfg, retrievers, client, cache), nil
 }
 
-func newService(retrievers []ff_retriever.Retriever, ff *ffclient.GoFeatureFlag) *serviceImpl {
+func newService(cfg *config.Config, retrievers []ff_retriever.Retriever, ff *feature.Client, cache cache.Cache[model.RawVarResult]) *serviceImpl {
 	return &serviceImpl{
+		cfg:        cfg,
 		ff:         ff,
 		retrievers: retrievers,
+		cache:      cache,
 	}
 }
 
-func (impl *serviceImpl) AppendOrUpdateRule(ctx context.Context, key string, rule *feature.Rule) error {
+func (impl *serviceImpl) AppendOrUpdateRule(ctx context.Context, key string, rule *domain_feature.Rule) error {
 	if err := validator.Validate(&key, validation.Required); err != nil {
 		return err
 	}
 	updaters := retriever.FindUpdateRetriever(impl.retrievers...)
+	type update struct {
+		variationResults interface{}
+		err              error
+	}
+
 	wg := sync.WaitGroup{}
-	errCh := make(chan error)
+	updaterValueCh := make(chan update)
+	done := make(chan struct{})
 	go func() {
 		wg.Wait()
-		close(errCh)
+		close(done)
 	}()
+
 	for _, updater := range updaters {
 		if updater.CanUpdate(ctx) {
 			wg.Add(1)
-			go func(ctx context.Context, updater retriever.UpdateRetriever, key string, rule *feature.Rule) {
+			go func(ctx context.Context, updater retriever.UpdateRetriever, key string, rule *domain_feature.Rule) {
 				defer wg.Done()
-				if err := updater.AppendOrUpdateRule(ctx, key, rule); err != nil {
-					errCh <- err
+				var updateResult update
+				if result := updater.GetVariationResult(ctx, key, rule.VariationResult); result != nil {
+					updateResult.variationResults = result
 				}
+				if err := updater.AppendOrUpdateRule(ctx, key, rule); err != nil {
+					updateResult.err = err
+				}
+				updaterValueCh <- updateResult
 			}(ctx, updater, key, rule)
 		}
 	}
-	for err := range errCh {
-		return err
+
+	updaterValues := make([]update, 0, len(updaters))
+flagLoop:
+	for {
+		select {
+		case <-done:
+			close(updaterValueCh)
+			break flagLoop
+		case v, ok := <-updaterValueCh:
+			if ok {
+				updaterValues = append(updaterValues, v)
+			}
+		}
 	}
-	return nil
+	for _, update := range updaterValues {
+		if update.err != nil {
+			return update.err
+		}
+	}
+	return impl.ff.Reset()
 }
 
-func (impl *serviceImpl) EvaluateFlag(ctx context.Context, key string, evalCtx feature.Context) (*model.RawVarResult, error) {
+func (impl *serviceImpl) evaluateFlag(key string, evalCtx domain_feature.Context) (*model.RawVarResult, error) {
 	defaultValue := "thisisadefaultvaluethatItest1233%%"
 	val, _ := impl.ff.RawVariation(key, evalCtx, defaultValue)
 	if val.Reason == internal.ReasonError {
 		msg := fmt.Sprintf("Error while evaluating the flag: %s", key)
-		return nil, feature.NewGeneralError(val.ErrorCode, msg, key)
+		return nil, domain_feature.NewGeneralError(val.ErrorCode, msg, key)
 	}
 	return &val, nil
 }
 
-func (impl *serviceImpl) EvaluateFlagsBulk(ctx context.Context, evalCtx feature.Context) map[string]feature.FlagState {
+func (impl *serviceImpl) EvaluateFlag(ctx context.Context, key string, evalCtx domain_feature.Context) (*model.RawVarResult, error) {
+	val, _ := impl.evaluateFlag(key, evalCtx)
+	if val.Reason == internal.ReasonError {
+		msg := fmt.Sprintf("Error while evaluating the flag: %s", key)
+		return nil, domain_feature.NewGeneralError(val.ErrorCode, msg, key)
+	}
+	return val, nil
+}
+
+func (impl *serviceImpl) EvaluateFlagsBulk(ctx context.Context, evalCtx domain_feature.Context) map[string]domain_feature.FlagState {
 	allFlags := impl.ff.AllFlagsState(evalCtx)
 	flags := allFlags.GetFlags()
-	results := make(map[string]feature.FlagState)
+	results := make(map[string]domain_feature.FlagState)
 	for key, val := range flags {
 		value := val.Value
 		if val.Reason == internal.ReasonError {
 			value = nil
 		}
-		results[key] = feature.FlagState{
+		results[key] = domain_feature.FlagState{
 			Value:         value,
 			Timestamp:     val.Timestamp,
 			VariationType: val.VariationType,
